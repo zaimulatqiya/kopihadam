78cbecfc4789fbc5e91dd42574d0cd71a9558b95
redigadaf <redigamaulana@gmail.com>
2026-01-24 20:24:05 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Navbar from "../components/layout/Navbar";
-import HeroBlend from "../components/sections/HeroBlend";
 import Hero from "../components/sections/Hero";
 import HowItWorks from "../components/sections/HowItWorks";
 import Footer from "../components/layout/Footer";
